4f34f01bb058ed51ff96e3aa61655fc765e39c08
Eudes <eudesyelyaore@gmail.com>
2023-05-12 18:14:43 +0000

    first commit

---
+++ Script/index.js
@@ -0,0 +1,5 @@
+const signinbtn=document.querySelector('signinbtn');
+const signupbtn=document.querySelector('signinbtn');
+const formbox=document.querySelector('.formbox');
+signinbtn.onclick=function(){
+    formbox.classList.add('active');}
